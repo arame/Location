e96595c545fe96ec19cb7237622dd05336d6b43f
Geoffrey Payne <hijiki7777@yahoo.co.uk>
2021-08-21 19:55:16 +0100

    Correct output fields names in config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import re
 class Hyper:
     UseUserLocation = False
     MustTranslate = False
-    _time = "2021_08_01 16_16_57"
+    _time = "2021_08_18 10_54_32"
     language = "en"
     UserLocationFile = "../Lookup/UserLocations.csv"
     HyrdatedTweetDirNoCountry = f"../Summary_Details_files{_time}/{language}/no_country"
@@ -13,7 +13,7 @@ class Hyper:
     HyrdatedTweetLangFile = f"{language}_tweets.csv"
     no_language_cnt = 0
     tweet_saved_cnt = 0
-    field_names = ['Id', 'Language', 'Place', 'User Location', 'Country', 'Full Text', 'Tweet', 'English Tweet', 'Retweet Count', 'Favourite Count']
+    field_names = ['Id', 'Language', 'Place', 'User Location', 'Country', 'Full Text', 'clean_text', 'Retweet Count', 'Favourite Count']
     
     def __init__(self) -> None:
         self.dirOutput = f"../Summary_Details_files{Hyper._time}"
